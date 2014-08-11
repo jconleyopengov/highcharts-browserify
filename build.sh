ccd45db0a438b6ae5836c33d04df7df75ec9d92b
John Conley <johnconley@OpenGovs-MacBook-Pro-18.local>
2014-08-11 14:23:36 -0700

    Update highcharts version

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 
 cat ./servershim.js > index.js
 cat ./highcharts.js >> index.js
-echo "console.log(window); module.exports = window.Highcharts;" >> index.js
-cat ./highcharts.js > browser.js
-echo "module.exports = window.Highcharts;" >> browser.js
+cat ./hc-exporting.js >> index.js
+echo "module.exports = window.Highcharts;" >> index.js
+
+echo "var $ = require('jquery');" > browser.js
+cat ./highcharts.js >> browser.js
+cat ./hc-exporting.js >> browser.js
+
+#export patched jquery along with highcharts
+echo "module.exports = window.Highcharts; module.exports.$ = $;" >> browser.js
